ab7509f67ad520144ba66a29b6c0665ca1bbc417
NoeAstoul <127491028+NoeAstoul@users.noreply.github.com>
2026-03-10 09:50:05 +0100

    ascenceur_animation

--- Assets/$_CleanUpTheWorms/Noe/Scripts/S_ElvatorDoorAnimation.cs
+++ Assets/$_CleanUpTheWorms/Noe/Scripts/S_ElvatorDoorAnimation.cs
@@ -2,31 +2,10 @@ using UnityEngine;
 
 public class S_ElvatorDoorAnimation : MonoBehaviour
 {
-    public Vector3 startPos;
-    public Vector3 endPos;
+    public Animator animator;
 
-    public float speed;
-    public float damping = 1f;
-
-    [Header("door animation")]
-    public GameObject door;
-    public Vector3 doorStartPos;
-    public Vector3 doorEndPos;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void switchAnim()
     {
-        startPos = transform.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = Vector3.Lerp(transform.position, endPos, damping * Time.deltaTime);
-
-        if(Vector3.Distance(transform.position, endPos) < 0.01f)
-        {
-            door.transform.localPosition = Vector3.Lerp(door.transform.localPosition, doorEndPos, damping * Time.deltaTime);
-        }
+        animator.SetBool("isTutoOver", true);
     }
 }
