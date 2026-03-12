de51b5bfedf5e9a645a8a6bf7993418ff9e5eed5
LaGeneze <56451934+LaGeneze@users.noreply.github.com>
2026-03-13 00:43:01 +0100

    APB

---
+++ Assets/$_CleanUpTheWorms/Emery/Scripts/StateMachine/S_BlendLight.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_BlendLight : MonoBehaviour
+{
+	public static S_BlendLight instance { get; private set; }
+
+	UnityEngine.Rendering.ProbeReferenceVolume probeRefVolume;
+	public string scenario01 = "Scenario01Name";
+	public string scenario02 = "Scenario02Name";
+	public string scenario03 = "Scenario03Name";
+	[Range(0, 1)] public float blendingFactor = 0.5f;
+	[Min(1)] public int numberOfCellsBlendedPerFrame = 10;
+
+	private void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this);
+		}
+		else
+		{
+			instance = this;
+		}
+	}
+
+	void Start()
+	{
+		probeRefVolume = UnityEngine.Rendering.ProbeReferenceVolume.instance;
+		probeRefVolume.lightingScenario = scenario01;
+		probeRefVolume.numberOfCellsBlendedPerFrame = numberOfCellsBlendedPerFrame;
+	}
+
+	public void SetScenario(string s)
+	{
+		switch (s)
+		{
+			case "Light On":
+				probeRefVolume.lightingScenario = scenario01;
+				break;
+			case "Light Off":
+				probeRefVolume.lightingScenario = scenario02;
+				break;
+			case "Light Mid":
+				probeRefVolume.lightingScenario = scenario03;
+				break;
+			default:
+				Debug.LogError("Invalid scenario index: " + s);
+				break;
+		}
+	}
+}
