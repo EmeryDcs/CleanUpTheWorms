a17abb5e515c3df77778e44d5ebae3b6e7e09950
LaGeneze <56451934+LaGeneze@users.noreply.github.com>
2026-08-31 17:05:12 +0200

    new throw system

--- Assets/$_CleanUpTheWorms/Noe/Scripts/grab.cs
+++ Assets/$_CleanUpTheWorms/Noe/Scripts/grab.cs
@@ -1,3 +1,6 @@
+using System;
+using Meta.XR.InputActions;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR;
@@ -25,10 +28,14 @@ public class grab : MonoBehaviour
     [SerializeField] private float triggerValue;
     private SphereCollider sphereCollider;
     
-    Vector3 controllerVelocity;
+    private Vector3 controllerVelocity;
     
-    private Vector3[] velocityHistory = new Vector3[5];
+    private Vector3[] velocityHistory = new Vector3[10];
     private int velocityHistoryIndex = 0;
+
+    private Rigidbody recentlyDetachedRb;
+    private float throwWindowTimer = 0f;
+    private float throwWindowDuration = 0.15f;
 
     private void Awake()
     {
@@ -54,23 +61,23 @@ public class grab : MonoBehaviour
 
     void Update()
     {
-        
         Vector3 currentVelocity;
         InputDevices.GetDeviceAtXRNode(controllerNode).TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceVelocity, out currentVelocity);
 
-        if (currentVelocity.sqrMagnitude > 0.01f)
-        {
-            velocityHistory[velocityHistoryIndex] = currentVelocity;
-            velocityHistoryIndex = (velocityHistoryIndex + 1) % velocityHistory.Length;
-        }
+        velocityHistory[velocityHistoryIndex] = currentVelocity;
+        velocityHistoryIndex = (velocityHistoryIndex + 1) % velocityHistory.Length;
 
-        controllerVelocity = Vector3.zero;
+        Vector3 bestVelocity = Vector3.zero;
+        float maxSqrMag = 0f;
         foreach (Vector3 v in velocityHistory)
         {
-            controllerVelocity += v;
+            if (v.sqrMagnitude > maxSqrMag)
+            {
+                maxSqrMag = v.sqrMagnitude;
+                bestVelocity = v;
+            }
         }
-        controllerVelocity /= velocityHistory.Length;
-        
+        controllerVelocity = bestVelocity;
         
         triggerValue = inputActions.Player.Grab.ReadValue<float>();
 
@@ -103,6 +110,16 @@ public class grab : MonoBehaviour
         {
             grabbedElm.transform.position = snapPoint.position;
         }
+
+        if (throwWindowTimer > 0f && recentlyDetachedRb != null)
+        {
+            throwWindowTimer -= Time.deltaTime;
+            
+            if (currentVelocity.sqrMagnitude > (recentlyDetachedRb.linearVelocity.sqrMagnitude / 4f))
+            {
+                recentlyDetachedRb.linearVelocity = currentVelocity * 2;
+            }
+        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -124,6 +141,9 @@ public class grab : MonoBehaviour
     private void GrabElm(GameObject elm)
     {
         InputDevices.GetDeviceAtXRNode(controllerNode).SendHapticImpulse(0u, 1f, 0.4f);
+        
+        recentlyDetachedRb = null;
+        throwWindowTimer = 0f;
 
         if (elm.GetComponent<AILarva>())
         {
@@ -132,16 +152,12 @@ public class grab : MonoBehaviour
             elm.GetComponent<AILarva>().StopBehaviorAndMakeKinematic();
             
             elm.GetComponent<S_GrabbableState>().SetIsGrabbed(true);
-
             elm.GetComponent<S_GrabbableState>().SetCanBeTrashed(true);
             
-
-
             return;
         }
 
         elm.GetComponent<S_GrabbableState>().SetIsGrabbed(true);
-
         elm.GetComponent<S_GrabbableState>().SetCanBeTrashed(true);
 
         grabbedElm = elm;
@@ -154,6 +170,8 @@ public class grab : MonoBehaviour
 
     private void DetachElm(GameObject elm)
     {
+        Rigidbody targetRb = null;
+
         if (elm.GetComponent<AILarva>())
         {
             elm.transform.SetParent(null);
@@ -162,28 +180,35 @@ public class grab : MonoBehaviour
             elm.GetComponent<AILarva>().RestartBehaviorAndMakeDynamic();
             elm.GetComponent<S_GrabbableState>().SetIsGrabbed(false);
             
-            Rigidbody larvaRb = elm.GetComponent<Rigidbody>();
-            if (larvaRb != null)
+            targetRb = elm.GetComponent<Rigidbody>();
+            if (targetRb != null)
             {
-                larvaRb.linearVelocity = controllerVelocity * 2;
+                targetRb.linearVelocity = controllerVelocity * 2;
+            }
+        }
+        else
+        {
+            targetRb = elm.GetComponent<Rigidbody>();
+
+            if (elm != null && elm.GetComponent<Collider>())
+            {
+                elm.GetComponent<Collider>().isTrigger = false;
+                elm.GetComponent<S_GrabbableState>().SetIsGrabbed(false);
             }
 
-            return;
+            targetRb.useGravity = true;
+            targetRb.isKinematic = false;
+            targetRb.linearVelocity = controllerVelocity * 2;
+            
+            elm.transform.SetParent(null);
+            grabbedElm = null;
         }
 
-        Rigidbody rb = grabbedElm.GetComponent<Rigidbody>();
-
-        if (elm != null && elm.GetComponent<Collider>())
+        if (targetRb != null)
         {
-            elm.GetComponent<Collider>().isTrigger = false;
-            elm.GetComponent<S_GrabbableState>().SetIsGrabbed(false);
+            recentlyDetachedRb = targetRb;
+            throwWindowTimer = throwWindowDuration;
         }
-
-        rb.useGravity = true;
-        rb.isKinematic = false;
-        rb.linearVelocity = controllerVelocity * 2;
-        elm.transform.SetParent(null);
-        grabbedElm = null;
     }
 
     public GameObject GetGrabbedElm()
